08cbfc48af5b1b2ac2581940fb6b5da3132e1f7b
yadavnagji-web <yadavnagji@gmail.com>
2025-12-19 21:36:43 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/Loveable-apps/",   // ✅ ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
